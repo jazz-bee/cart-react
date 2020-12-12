b0c0a47def47b9fb7e48440e6b96e37c1fe6be87
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2020-12-12 20:51:19 -0300

    refactor badges

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,23 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
   state={
-    count: 0
+    count: 4
+    
   };
   render(){
-    let classes = "badge m-2 badge-"
-    classes += this.state.count === 0 ? "warning" : "primary";
     
     return (
       <div>
-        <span className={classes}>{this.state.count}</span>
+        <span className={this.getBadgeClasses()}>{this.state.count}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     )
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 export default Counter;
