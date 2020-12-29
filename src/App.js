0b04a1186431a5c742a9539ad43f9a9774e3c6d6
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2020-12-29 01:57:57 -0300

    add totalCounters calculation in app

--- src/App.js
+++ src/App.js
@@ -40,9 +40,12 @@ handleReset = () => {
   render() { 
     return (  
       <React.Fragment>
-      <NavBar></NavBar>
+      <NavBar
+       totalCounters={this.state.counters.filter(c=> c.value > 0).length}
+      ></NavBar>
       <main className="container">
         <Counters
+        counters={this.state.counters}
         onReset={this.handleReset} 
         onDelete={this.handleDelete}
         onIncrement={this.handleIncrement}
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,51 +1,19 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 class Counters extends Component {
-    state = { 
-        counters: [
-           {id: 1,  value: 0},
-           {id: 2,  value: 0},
-           {id: 3,  value: 0},
-           {id: 4,  value: 0} 
-        ]
-     };
-     handleDelete = (counterId) => {
-        console.log("Event handler called", counterId);
-         const counters = this.state.counters.filter(c => c.id !== counterId);
-         this.setState({ counters }); //"counters: counters" is not necessary 
-         console.log(this.state.counters);
     
-    };
-
-    handleIncrement = (counter) => { 
-        //console.log(this.state.counters); 
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {... counter}; //the counter object we recieved as an argument
-        counters[index].value++;        // that way we dont modify the original object in the array
-        this.setState({counters});
-    };
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c =>{
-            c.value=0;
-            return c;
-        })
-        this.setState(counters);
-
-    };
     render() { 
         return ( 
         <div>
-            {this.state.counters.map(counter => 
+            {this.props.counters.map(counter => 
                 <Counter 
                 key={counter.id} 
-                onDelete={this.handleDelete} 
-                onIncrement={this.handleIncrement}
+                onDelete={this.props.onDelete} 
+                onIncrement={this.props.onIncrement}
                 counter = {counter} // we pass the object not the properties one by one
                 /> 
             )}
-            <button onClick={() => this.handleReset()} className="btn btn-sm" >Reset</button>
+            <button onClick={() => this.props.onReset()} className="btn btn-sm" >Reset</button>
         </div> 
         );
     }
