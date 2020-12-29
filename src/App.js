49fda616448003fb5927471624e3e3695746e12a
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2020-12-29 00:59:47 -0300

    update return and add wrapper Recat.Fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import navBar from './components/navbar'
+import React, { Component } from 'react';
+import NavBar from './components/navbar'
 import './App.css';
+import Counters from './components/counters';
 
 class App extends Component {
   state = {  }
   render() { 
     return (  
+      <React.Fragment>
       <NavBar></NavBar>
+      <main className="container">
+        <Counters/>
+      </main>
+      </React.Fragment>
     );
   }
 }
